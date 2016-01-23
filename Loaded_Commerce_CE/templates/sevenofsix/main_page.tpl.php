d619fac5522d08bec31f1c267837504b1cefb28f
Devi Dash <devi@webtenet.com>
2016-01-23 23:45:10 +0530

    #36 responsive template addition to the package

--- Loaded_Commerce_CE/templates/sevenofsix/main_page.tpl.php
+++ Loaded_Commerce_CE/templates/sevenofsix/main_page.tpl.php
@@ -5,8 +5,7 @@
   CRE Loaded, Open Source E-Commerce Solutions
   http://www.creloaded.com
 
-  Copyright (c) 2008 CRE Loaded
-  Copyright (c) 2008 AlogoZone, Inc.
+  Copyright (c) 2016 CRE Loaded
   Copyright (c) 2003 osCommerce
 
   Released under the GNU General Public License
