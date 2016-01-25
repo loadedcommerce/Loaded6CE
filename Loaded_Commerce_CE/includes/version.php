2d2d5cdb2b9c2addbd3996acd8eb9fda0fd354d0
Devi Dash <devi@webtenet.com>
2016-01-25 19:55:31 +0530

    Version updated to 6.5.3 MR

--- Loaded_Commerce_CE/includes/version.php
+++ Loaded_Commerce_CE/includes/version.php
@@ -9,6 +9,6 @@
   Released under the GNU General Public License
 */
 // define the project version
-define('PROJECT_VERSION', 'Loaded Commerce Community Edition v6.5.2');
+define('PROJECT_VERSION', 'Loaded Commerce Community Edition v6.5.3 MR');
 define('INSTALLED_VERSION_TYPE', 'Community Edition');
 ?>
