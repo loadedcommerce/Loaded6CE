4be9a3f25964ef36ea11dbe7f342b43574fd460c
Devi <devi@egcms.com>
2016-03-11 21:47:46 +0530

    Crosssell products and IPN page design issue fixing

--- Loaded_Commerce_CE/admin/xsell_products.php
+++ Loaded_Commerce_CE/admin/xsell_products.php
@@ -1,4 +1,6 @@
+
 <?php
+
 /*
   $Id: xsell_products.php,v 1.2 2008/06/13 00:18:17 datazen Exp $
 
@@ -67,33 +69,61 @@ switch($action){
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
 <title><?php echo TITLE; ?></title>
-<script type="text/javascript" src="<?php echo (($request_type == 'SSL') ? 'https:' : 'http:'); ?>//ajax.googleapis.com/ajax/libs/jquery/<?php echo JQUERY_VERSION; ?>/jquery.min.js"></script>
-<script type="text/javascript">
-  if (typeof jQuery == 'undefined') {
-    //alert('You are running a local copy of jQuery!');
-    document.write(unescape("%3Cscript src='includes/javascript/jquery-1.6.2.min.js' type='text/javascript'%3E%3C/script%3E"));
-  }
-</script>
-<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
-<!--[if IE]>
-<link rel="stylesheet" type="text/css" href="includes/stylesheet-ie.css">
-<![endif]-->
-<script language="javascript" src="includes/general.js"></script>
-<link rel="stylesheet" type="text/css" href="includes/headernavmenu.css">
-<script type="text/javascript" src="includes/menu.js"></script>
+<link rel="icon" type="image/png" href="favicon.ico" />
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+  <link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
+  <!-- ================== BEGIN BASE CSS STYLE ================== -->
+  <link href="<?php echo (($request_type == 'SSL') ? 'https:' : 'http:'); ?>//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <link href="assets/plugins/jquery-ui/themes/base/minified/jquery-ui.min.css" rel="stylesheet" />
+  <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+  <link href="assets/css/animate.min.css" rel="stylesheet" />
+  <link href="assets/css/style.min.css" rel="stylesheet" />
+  <link href="assets/css/style-responsive.min.css" rel="stylesheet" />
+  <link href="assets/css/theme/blue.css" rel="stylesheet" id="theme" />
+  <!-- ================== END BASE CSS STYLE ================== -->
+
+  <!-- ================== BEGIN PAGE LEVEL STYLE ================== -->
+  <link href="assets/plugins/jquery-jvectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet" />
+  <link href="assets/plugins/bootstrap-datepicker/css/datepicker.css" rel="stylesheet" />
+  <link href="assets/plugins/bootstrap-datepicker/css/datepicker3.css" rel="stylesheet" />
+    <link href="assets/plugins/gritter/css/jquery.gritter.css" rel="stylesheet" />
+    <link href="assets/plugins/DataTables/media/css/dataTables.bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/plugins/DataTables/extensions/Select/css/select.bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/plugins/DataTables/extensions/Responsive/css/responsive.bootstrap.min.css" rel="stylesheet" />
+  <!-- ================== END PAGE LEVEL STYLE ================== -->
+  <script language="javascript" src="includes/general.js"></script>
+  <script type="text/javascript" src="includes/menu.js"></script>
+  <?php require('includes/account_check.js.php'); ?>
 </head>
-<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
-<!-- header //-->
-<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
-<!-- header_eof //-->
-<!-- body //-->
-<div id="body">
-<table width="100%"  border="0" align="center" cellpadding="0" cellspacing="0" class="body-table">
-  <tr>
+</head>
+<body>
+    <!-- begin #page-container -->
+    <div id="page-container" class="fade page-sidebar-fixed page-header-fixed gradient-enabled">
+    <!-- header //-->
+    <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+    <!-- header_eof //-->
+
     <!-- left_navigation //-->
     <?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
     <!-- left_navigation_eof //-->
-    <!-- body_text //-->
+
+    <!-- begin #content -->
+    <div id="content" class="content">
+      <!-- begin breadcrumb -->
+      <ol class="breadcrumb pull-right">
+        <li>Create &nbsp; <a title="<?php echo BOX_MANUAL_ORDER_CREATE_ACCOUNT;?>" href="<?php echo tep_href_link(FILENAME_CREATE_ACCOUNT,'','SSL');?>" class="btn btn-xs btn-header"><i class="fa fa-user"></i><span class="label">+</span></a> <a title="<?php echo BOX_MANUAL_ORDER_CREATE_ORDER;?>" href="<?php echo tep_href_link(FILENAME_CREATE_ORDER,'','SSL');?>" class="btn btn-xs btn-header"><i class="fa fa-shopping-cart"></i><span class="label">+</span></a></li>
+        <li>Search &nbsp; <a href="javascript:;" class="btn btn-header btn-xs header-popover" id="ProductsPopover">Products</a> <a href="javascript:;" class="btn btn-header btn-xs header-popover" id="CustomerPopover">Customers</a> <a href="javascript:;" class="btn btn-header btn-xs header-popover" id="OrdersPopover">Orders</a> <a href="javascript:;" class="btn btn-header btn-xs header-popover" id="PagesPopover">Pages</a></li>
+      </ol>
+      <!-- end breadcrumb -->
+      <!-- begin page-header -->
+      <h1 class="page-header"><?php echo HEADING_TITLE; ?></h1>
+      <!-- end page-header -->
+
+    <!-- begin panel -->
+    <div class="panel panel-inverse">
+<!-- header_eof //-->
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
     <td valign="top" class="page-container">
       <table border="0" width="100%" cellspacing="0" cellpadding="0">
         <tr>
@@ -141,16 +171,16 @@ switch($action){
           </tr>
           <?php
           if (isset($_POST['search'])) {
-            $search = str_replace("'", "&#39", tep_db_prepare_input($_POST['search'])); 
-            $products_query_raw = "SELECT p.products_id, 
-                                          p.products_model, 
-                                          pd.products_name, 
+            $search = str_replace("'", "&#39", tep_db_prepare_input($_POST['search']));
+            $products_query_raw = "SELECT p.products_id,
+                                          p.products_model,
+                                          pd.products_name,
                                           p.products_id
-                                     FROM " . TABLE_PRODUCTS . " p, 
-                                          " . TABLE_PRODUCTS_DESCRIPTION . " pd 
-                                    WHERE p.products_id = pd.products_id 
-                                      and (pd.products_name like '%" . tep_db_input($search) . "%' or p.products_model like '%" . tep_db_input($search) . "%') 
-                                      and pd.language_id = '" . (int)$languages_id . "' 
+                                     FROM " . TABLE_PRODUCTS . " p,
+                                          " . TABLE_PRODUCTS_DESCRIPTION . " pd
+                                    WHERE p.products_id = pd.products_id
+                                      and (pd.products_name like '%" . tep_db_input($search) . "%' or p.products_model like '%" . tep_db_input($search) . "%')
+                                      and pd.language_id = '" . (int)$languages_id . "'
                                  ORDER BY p.products_model, pd.products_name ASC";
           } else {
             $products_query_raw = 'select p.products_id, p.products_model, pd.products_name, p.products_id from '.TABLE_PRODUCTS.' p, '.TABLE_PRODUCTS_DESCRIPTION.' pd where p.products_id = pd.products_id and pd.language_id = "'.(int)$languages_id.'" order by p.products_id asc';
@@ -231,11 +261,11 @@ switch($action){
           </tr>
           <tr>
             <td>
-              <?php                 
+              <?php
                 $products_cross_image_query = tep_db_query('select p.products_id, p.products_model, p.products_image, p.products_id, x.products_id, x.xsell_id, x.sort_order, x.ID from '.TABLE_PRODUCTS.' p,  '.TABLE_PRODUCTS_XSELL.' x where x.xsell_id = p.products_id and x.products_id = "'.$_GET['add_related_product_ID'].'" order by x.sort_order asc');
                 echo '<table border ="0"  cellpadding = "0" cellspacing = "0"><tr>';
                 $i = 0;
-                 while ($products_cross_image = tep_db_fetch_array($products_cross_image_query)){                       
+                 while ($products_cross_image = tep_db_fetch_array($products_cross_image_query)){
                  echo '<td>'.tep_image( DIR_WS_CATALOG_IMAGES . $products_cross_image['products_image'],$products_name['products_name'],SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, 'hspace="5" vspace="5"').'</td>';
                  if($i == 6){
                    echo '</tr><tr>';
@@ -294,7 +324,7 @@ switch($action){
                     <td class="smallText" align="right"><?php echo $products_split->display_links($products_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, MAX_DISPLAY_PAGE_LINKS, $page, tep_get_all_get_params(array('page', 'info', 'x', 'y', 'cID', 'action'))); ?></td>
                   </tr>
                 </table></td>
-              </tr> 
+              </tr>
             </table></td>
           </tr>
           <?php
@@ -304,7 +334,7 @@ switch($action){
           ?>
         </table>
         <?php
-      } elseif ( (isset($_GET['add_related_product_ID']) && $_GET['add_related_product_ID'] != '') && (isset($_GET['sort']) && $_GET['sort'] != '') ){ 
+      } elseif ( (isset($_GET['add_related_product_ID']) && $_GET['add_related_product_ID'] != '') && (isset($_GET['sort']) && $_GET['sort'] != '') ){
         $products_name_query = tep_db_query('select pd.products_name, p.products_model, p.products_image from '.TABLE_PRODUCTS.' p, '.TABLE_PRODUCTS_DESCRIPTION.' pd where p.products_id = "'.$_GET['add_related_product_ID'].'" and p.products_id = pd.products_id and pd.language_id ="'.(int)$languages_id.'"');
         $products_name = tep_db_fetch_array($products_name_query);
         ?>
@@ -318,6 +348,9 @@ switch($action){
                   </tr>
                 </table></td>
               </tr>
+ 		<div class="table-responsive">
+			<table class="table table-bordered">
+			 <thead>
               <tr class="dataTableHeadingRow">
                 <td class="dataTableHeadingContent" width="15%">&nbsp;<?php echo TABLE_HEADING_PRODUCT_ID;?>&nbsp;</td>
                 <td class="dataTableHeadingContent" width="15%">&nbsp;<?php echo TABLE_HEADING_PRODUCT_MODEL;?>&nbsp;</td>
@@ -326,6 +359,8 @@ switch($action){
                 <td class="dataTableHeadingContent" width="15%">&nbsp;<?php echo TABLE_HEADING_PRODUCT_PRICE;?>&nbsp;</td>
                 <td class="dataTableHeadingContent" width="15%">&nbsp;<?php echo TABLE_HEADING_PRODUCT_SORT;?>&nbsp;</td>
               </tr>
+             </thead>
+             <tbody>
               <?php
               $products_query_raw = 'select p.products_id as products_id, p.products_price, p.products_image, p.products_model, pd.products_name, p.products_id, x.products_id as xproducts_id, x.xsell_id, x.sort_order, x.ID from '.TABLE_PRODUCTS.' p, '.TABLE_PRODUCTS_DESCRIPTION.' pd, '.TABLE_PRODUCTS_XSELL.' x where x.xsell_id = p.products_id and x.products_id = "'.$_GET['add_related_product_ID'].'" and p.products_id = pd.products_id and pd.language_id = "'.(int)$languages_id.'" order by x.sort_order asc';
               $products_split = new splitPageResults($page, MAX_DISPLAY_SEARCH_RESULTS, $products_query_raw, $products_query_numrows);
@@ -347,8 +382,10 @@ switch($action){
                 <?php
               }
               ?>
-              <tr>
-                <td colspan="6"><table border="0" width="100%" cellspacing="0" cellpadding="2" class="infoBoxContent">
+				</tbody>
+              </table>
+             </div>
+                <table border="0" width="100%" cellspacing="0" cellpadding="2" class="infoBoxContent">
                   <tr>
                     <td class="smallText" valign="top"><?php echo $products_split->display_count($products_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, $page, TEXT_DISPLAY_NUMBER_OF_PRODUCTS); ?></td>
                     <td class="smallText" align="right"><?php echo $products_split->display_links($products_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, MAX_DISPLAY_PAGE_LINKS, $page, tep_get_all_get_params(array('page', 'info', 'x', 'y', 'cID', 'action'))); ?></td>
@@ -371,18 +408,17 @@ switch($action){
           ?>
         </table>
         <?php
-        }        
+        }
       ?>
-      <!-- body_text_eof //-->
-    </td>
   </tr>
 </table>
 </div>
 <!-- body_eof //-->
+
 <!-- footer //-->
-<?php include(DIR_WS_INCLUDES . 'footer.php'); ?>
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
 <!-- footer_eof //-->
 <br>
 </body>
 </html>
-<?php include(DIR_WS_INCLUDES . 'application_bottom.php');?>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
