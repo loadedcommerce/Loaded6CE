f1718f1857516d3a9aa53c51a4e940b94286f375
cord <cf@lct.de>
2016-05-02 11:22:39 +0200

    added new MailBeez core installer files

---
+++ Loaded_Commerce_CE/mailhive/cloudbeez/dev_environment.php
@@ -0,0 +1,10 @@
+<?php
+define('CLOUDLOADER_DISABLE_DEPLOY', false);
+//define('CLOUDLOADER_URL', 'http://cloudbeez');
+define('CLOUDLOADER_URL', 'http://staging.cloudbeez.com');
+
+
+//define('CLOUDLOADER_API_KEY', '798d10538a76b365423030bedcfe8e16833b4cff');
+
+
+?>
