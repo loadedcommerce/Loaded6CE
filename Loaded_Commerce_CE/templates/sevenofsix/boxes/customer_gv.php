bc10344306d7102107be72fed906ddc7d82d4ef9
Devi <devi@egcms.com>
2016-06-07 17:14:01 +0530

    #76 Customer gv infobox stying issue fixing

--- Loaded_Commerce_CE/templates/sevenofsix/boxes/customer_gv.php
+++ Loaded_Commerce_CE/templates/sevenofsix/boxes/customer_gv.php
@@ -18,29 +18,15 @@ $customer_gv = new box_customer_gv();
 
 if ($customer_gv->amount > 0) {
 ?>
-  <tr>
-    <td>
-      <?php
-      $info_box_contents = array();
-      $info_box_contents[] = array('align' => 'left',
-                                   'text'  => '<font color="' . $font_color . '">' . BOX_HEADING_GIFT_VOUCHER . '</font>'
-                                  );
-      new $infobox_template_heading($info_box_contents, '', ((isset($column_location) && $column_location !='') ? $column_location : '') );
-      $info_box_contents = array();
-      $info_box_contents[] = array('align' => 'left',
-                                   'text'  => '<div align="center">'.GIFT_VOUCHER_ACCOUNT_BALANCE_1.$currencies->format($customer_gv->amount).GIFT_VOUCHER_ACCOUNT_BALANCE_2.'<a href="'.tep_href_link(FILENAME_GV_SEND).'">'.GIFT_VOUCHER_ACCOUNT_BALANCE_3.'</a>'
-                                  );
-      new $infobox_template($info_box_contents, true, true, ((isset($column_location) && $column_location !='') ? $column_location : '') );
-      if (TEMPLATE_INCLUDE_FOOTER =='true') {
-        $info_box_contents = array();
-        $info_box_contents[] = array('align' => 'left',
-                                     'text'  => tep_draw_separator('pixel_trans.gif', '100%', '1')
-                                    );
-        new $infobox_template_footer($info_box_contents, ((isset($column_location) && $column_location !='') ? $column_location : '') );
-      }
-      ?>
-    </td>
-  </tr>
+<div class="well" >
+  <ul class="box-shopping-cart list-unstyled list-indent-large">
+    <li class="box-header small-margin-bottom"><?php     echo  BOX_HEADING_GIFT_VOUCHER ; ?></li>
+<?php
+echo '<li><div align="center">'.GIFT_VOUCHER_ACCOUNT_BALANCE_1.$currencies->format($customer_gv->amount).GIFT_VOUCHER_ACCOUNT_BALANCE_2.'<a href="'.tep_href_link(FILENAME_GV_SEND).'">'.GIFT_VOUCHER_ACCOUNT_BALANCE_3.'</a><div></li>';
+?>
+  </ul>
+</div>
+
 <?php
 }
 ?>
