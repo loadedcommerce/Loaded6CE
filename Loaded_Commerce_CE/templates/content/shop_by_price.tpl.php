67e00fcf75975a12d369f7f5e4e5aad23e15b334
Devi <devi@egcms.com>
2016-06-07 16:55:24 +0530

    Shop by Price issue fixing

--- Loaded_Commerce_CE/templates/content/shop_by_price.tpl.php
+++ Loaded_Commerce_CE/templates/content/shop_by_price.tpl.php
@@ -4,61 +4,32 @@
 echo $cre_RCI->get('shopbyprice', 'top');
 // RCI code eof
 ?>
-<table border="0" width="100%" cellspacing="0" cellpadding="<?php echo CELLPADDING_SUB; ?>">
+ <div class="row">
+   <div class="col-sm-12 col-lg-12 large-padding-left margin-top">
 <?php
 // BOF: Lango Added for template MOD
 if (SHOW_HEADING_TITLE_ORIGINAL == 'yes') {
 $header_text = '&nbsp;'
 //EOF: Lango Added for template MOD
 ?>
-      <tr>
-        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
-          <tr>
-            <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
-            <td class="pageHeading" align="right"><?php echo tep_image(DIR_WS_IMAGES . 'table_background_products_new.gif', HEADING_TITLE, HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
-          </tr>
-        </table></td>
-      </tr>
-      <tr>
-        <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
-      </tr>
+  <h1 class="no-margin-top"><?php echo HEADING_TITLE; ?></h1>
 <?php
 // BOF: Lango Added for template MOD
 }else{
 $header_text = HEADING_TITLE;
 }
 // EOF: Lango Added for template MOD
-?>
-<?php
-// BOF: Lango Added for template MOD
-if (MAIN_TABLE_BORDER == 'yes'){
-table_image_border_top(false, false, $header_text);
-}
-// EOF: Lango Added for template MOD
-?>
-      <tr>
-        <td>
-<?php
 //include(DIR_WS_MODULES . FILENAME_PRODUCT_LISTING);
      if ( file_exists(TEMPLATE_FS_CUSTOM_MODULES . FILENAME_PRODUCT_LISTING)) {
         require(TEMPLATE_FS_CUSTOM_MODULES . FILENAME_PRODUCT_LISTING);
     } else {
         require(DIR_WS_MODULES . FILENAME_PRODUCT_LISTING);
     }
-?>
-        </td>
-      </tr>
-<?php
 // RCI code start
 echo $cre_RCI->get('shopbyprice', 'menu');
 // RCI code eof
-// BOF: Lango Added for template MOD
-if (MAIN_TABLE_BORDER == 'yes'){
-table_image_border_bottom();
-}
-// EOF: Lango Added for template MOD
 ?>
-    </table>
+    </div></div>
 <?php 
 // RCI code start
 echo $cre_RCI->get('shopbyprice', 'bottom');
